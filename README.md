4b0e12e8f3e546aa76d17362b75985825c9ac6b1
erfanio <dev@erfan.io>
2020-04-04 06:52:37 +0000

    add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Server setup
+- Run `wget https://gitlab.com/erfanio/server_setup/-/raw/master/init.sh -O init.sh`
+- Inspect `init.sh` and run it `bash init.sh INSERT_USERNAME` (username is used to create a new user and the script will disable the root user)
+
--- init.sh
+++ init.sh
@@ -1,14 +1,21 @@
+NEW_USER=${1:-erfan}
+
 echo "Install ZSH"
 sudo apt update && sudo apt install zsh
 
-echo "Create erfan user"
-sudo adduser --system --group erfan
+echo "Create new user $NEW_USER"
+sudo adduser --system --group $NEW_USER
+echo "Enter a password for the new user"
+sudo passwd $NEW_USER
+# disable root login
+sudo usermod --shell /usr/sbin/nologin root
+
 
 echo "Clone dotfiles"
-git clone https://github.com/erfanio/dotfiles.git /home/erfan/dotfiles
-git clone https://gitlab.com/erfanio/server_setup.git /home/erfan/server_setup
-chown erfan:erfan /home/erfan -R
+git clone https://github.com/erfanio/dotfiles.git /home/$NEW_USER/dotfiles
+git clone https://gitlab.com/erfanio/server_setup.git /home/$NEW_USER/server_setup
+chown $NEW_USER:$NEW_USER /home/$NEW_USER -R
 
-echo "Copying ssh keys from root to erfan"
-mkdir -p /home/erfan/.ssh
-cp /root/.ssh/authorized_keys /home/erfan/.ssh/authorized_keys
+echo "Copying ssh keys from root to $NEW_USER"
+mkdir -p /home/$NEW_USER/.ssh
+cp /root/.ssh/authorized_keys /home/$NEW_USER/.ssh/authorized_keys
